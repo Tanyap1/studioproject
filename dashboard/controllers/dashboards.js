9f00ec4a5c43f578a670577794f455603304e418
github-Tanyap1 <github-Tanya.Pachkalov@MyGeorgian.ca>
2022-03-30 11:55:11 -0400

    some footer adj

--- dashboard/controllers/dashboards.js
+++ dashboard/controllers/dashboards.js
@@ -2,7 +2,7 @@ const express=require('express')
 const router=express.Router()
 
 router.get( '/', (req,res)=>{
-    res.render('employers/index', {title:'Dash Board'})
+    res.render('dashboards/index', {title:'Dash Board'})
 })
 
 
